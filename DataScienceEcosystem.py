a3e1292eb72b6737a51b313bc0409d56acf32438
taiwoariyo <162063747+taiwoariyo@users.noreply.github.com>
2024-03-06 13:28:07 -0600

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# This notebook includes a summary of common tools and libraries useful for data science.
+
+# **Objectives:**
+# - List data science languages.
+# - List data science libraries.
+# - Data Science tools.
+# - Creating tables in Jupyter notebook.
+# - Creating lists in Juputer notebook.
+
+# A few languages commonly used by data scientists around the world today are:
+# 
+# 1. Python.
+# 2. Java.
+# 3. Scala.
+# 4. C++
+# 5. MATLAB
+
+# Data science libraries commonly used in the field include:
+#     
+# 1. Pandas.
+# 2. Numpy.
+# 3. Matplotlib.
+# 4. Seaborn.
+# 5. Scikit-Learn.
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook   |      
+# | Jupyter Lab        |
+# | V.S Code           |
+
+# Below are are some examples showing Arithmetic evaluation of expressions in Python
+
+# In[1]:
+
+
+#This is a simple Arithmetic expression to multiply and add integers
+
+(3*4)+5
+
+
+# In[4]:
+
+
+#This expression would convert 200 minutes into hours by dividing the given nunmber of minutes by 60
+
+Minutes=200
+Hours= Minutes/60
+print(Hours)
+
+
+# ## Author
+# 
+# Taiwo Ariyo
